358588b9e8076a4d3e4fe195b9998b39eb0ee37e
Syon <ikbensyon@gmail.com>
2019-06-11 14:01:04 +0200

    Fixed The Project

---
+++ Project_Alumni/Project_Alumni/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Alumni.Startup))]
+namespace Project_Alumni
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

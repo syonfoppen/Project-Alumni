b76dc848cc498ac712eda180a7e5dfb12f66354d
Syon <ikbensyon@gmail.com>
2019-06-12 12:56:57 +0200

    RESTART AGAIN

--- Project_Alumni/ProjectAlumni/Properties/AssemblyInfo.cs
+++ Project_Alumni/ProjectAlumni/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59269e90-0ffd-428a-b3f9-70622bc5f82e")]
+[assembly: Guid("f7015cfb-c498-4462-9d72-a2e577e4d67c")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Project_Alumni/ProjectAlumni/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectAlumni.Startup))]
+namespace ProjectAlumni
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

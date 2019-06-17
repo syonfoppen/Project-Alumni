fb973125b68539a92c6357f70b3dfa358d3acbf7
Syon <ikbensyon@gmail.com>
2019-06-17 10:16:56 +0200

    Added keys to the moddels

--- Project_Alumni/AlumniDB/cours.cs
+++ Project_Alumni/AlumniDB/cours.cs
@@ -11,9 +11,11 @@ namespace AlumniDB
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class cours
     {
+        [Key]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public cours()
         {

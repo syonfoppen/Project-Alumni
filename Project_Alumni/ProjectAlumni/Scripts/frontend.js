41a59ed6ec2c51e13bc5f29dc23b0f4d94982591
Unstopped <44770851+Unstopped@users.noreply.github.com>
2019-06-18 15:05:09 +0200

    made the frontend navbar and added some files

---
+++ Project_Alumni/ProjectAlumni/Scripts/frontend.js
@@ -0,0 +1,13 @@
+﻿let x = false; 
+function nav() {
+	if (x == false) {
+		document.getElementById("navbar").style.transition = "right 0.25s";
+		document.getElementById("navbar").style.right = "-450px";
+		x = true;
+	}
+	else {
+		document.getElementById("navbar").style.transition = "right 0.25s";
+		document.getElementById("navbar").style.right = "-10px";
+		x = false
+	}
+}

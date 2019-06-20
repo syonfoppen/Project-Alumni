e04e1a55c37e41176abb103b73b1cfbc19992b9b
Emanuel de Jong <manuelwindowslive@hotmail.com>
2019-06-20 12:49:59 +0200

    changed admin tools

---
+++ Project_Alumni/ProjectAlumni/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAlumni.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult redirectRoles()
+        {
+            return RedirectToAction("Index", "RolesController");
+        }
+    }
+}

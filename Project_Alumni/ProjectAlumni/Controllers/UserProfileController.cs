8bf3abe062f18aa6b6d8c9f978a8784b621b6157
Emanuel de Jong <manuelwindowslive@hotmail.com>
2019-06-27 12:08:31 +0200

    update on profile doesn't overwrite EVERYTHING anymore

--- Project_Alumni/ProjectAlumni/Controllers/UserProfileController.cs
+++ Project_Alumni/ProjectAlumni/Controllers/UserProfileController.cs
@@ -14,18 +14,10 @@ namespace ProjectAlumni.Controllers
     public class UserProfileController : Controller
     {
         private DatabaseEntities db = new DatabaseEntities();
-
-        // GET: UserProfile
-        public ActionResult Index()
-        {
-            var username = User.Identity.Name;
-            var userid = db.AspNetUsers.SqlQuery("SELECT * FROM AspNetUsers WHERE UserName = " + "'" + username + "'").ToList();
-            AspNetUser user = userid[0];
-            return RedirectToAction("Edit");
-        }
+        
         
         // GET: UserProfile/Edit/5
-        public ActionResult Edit()
+        public ActionResult Index()
         {
             var username = User.Identity.Name;
             var userid = db.AspNetUsers.SqlQuery("SELECT * FROM AspNetUsers WHERE UserName = " + "'" + username + "'").ToList();
@@ -39,7 +31,6 @@ namespace ProjectAlumni.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.AdressId = new SelectList(db.addresses, "addressid", "country", aspNetUser.AdressId);
             ViewBag.GenderId = new SelectList(db.genders, "genderid", "NAME", aspNetUser.GenderId);
             return View(aspNetUser);
         }
@@ -51,32 +42,69 @@ namespace ProjectAlumni.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Email,PhoneNumber,UserName,ProfilePicture,AdressId,GenderId,Firstname,Lastname,DateOfBirth,Description,GraduationYear,ProfilePicture")] AspNetUser aspNetUser, HttpPostedFileBase upload)
         {
+            AspNetUser currentUser = new AspNetUser();
             if (ModelState.IsValid)
             {
+                byte[] profilePicture = null;
                 if (upload != null && upload.ContentLength > 0)
                 {
 
                     using (var reader = new System.IO.BinaryReader(upload.InputStream))
                     {
-                        aspNetUser.ProfilePicture = reader.ReadBytes(upload.ContentLength);
+                         profilePicture = reader.ReadBytes(upload.ContentLength);
                     }
                 }
-                db.Entry(aspNetUser).State = EntityState.Modified;
+
+                currentUser = db.AspNetUsers.FirstOrDefault(p => p.Id == aspNetUser.Id);
+                if (currentUser == null)
+                    return HttpNotFound();
+
+                if (profilePicture != null)
+                {
+                    currentUser.ProfilePicture = profilePicture;
+                }
+                if (aspNetUser.Email != String.Empty && aspNetUser.Email != "" && aspNetUser.Email != null)
+                {
+                    currentUser.Email = aspNetUser.Email;
+                }
+                if (aspNetUser.PhoneNumber != String.Empty && aspNetUser.PhoneNumber != "" && aspNetUser.PhoneNumber != null)
+                {
+                    currentUser.PhoneNumber = aspNetUser.PhoneNumber;
+                }
+                if (aspNetUser.UserName != String.Empty && aspNetUser.UserName != "" && aspNetUser.UserName != null)
+                {
+                    currentUser.UserName = aspNetUser.UserName;
+                }
+                if (aspNetUser.Firstname != String.Empty && aspNetUser.Firstname != "" && aspNetUser.Firstname != null)
+                {
+                    currentUser.Firstname = aspNetUser.Firstname;
+                }
+                if (aspNetUser.Lastname != String.Empty && aspNetUser.Lastname != "" && aspNetUser.Lastname != null)
+                {
+                    currentUser.Lastname = aspNetUser.Lastname;
+                }
+                if (aspNetUser.DateOfBirth != null)
+                {
+                    currentUser.DateOfBirth = aspNetUser.DateOfBirth;
+                }
+                if (aspNetUser.Description != String.Empty && aspNetUser.Description != "" && aspNetUser.Description != null)
+                {
+                    currentUser.Description = aspNetUser.Description;
+                }
+                if (aspNetUser.GraduationYear != 0 && aspNetUser.GraduationYear != null)
+                {
+                    currentUser.GraduationYear = aspNetUser.GraduationYear;
+                }
+                if (aspNetUser.GenderId != 0 && aspNetUser.GenderId != null)
+                {
+                    currentUser.GenderId = aspNetUser.GenderId;
+                }
+
+                db.Entry(currentUser).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.AdressId = new SelectList(db.addresses, "addressid", "country", aspNetUser.AdressId);
-            ViewBag.GenderId = new SelectList(db.genders, "genderid", "NAME", aspNetUser.GenderId);
-            return View(aspNetUser);
-        }
-        
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return RedirectToAction("Index");
         }
     }
 }
